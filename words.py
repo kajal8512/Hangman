6bd56c34d43b87ffc11a8adf25a27146a923cb3a
Kajal Yadav <kajaly20@navgurukul.org>
2021-08-09 01:32:21 +0530

    initial commit

---
+++ words.py
@@ -0,0 +1,24 @@
+import string
+import random
+
+def load_words():
+    """
+    Ye function kaafi jayada words ko load karne mai help karega
+    """
+    WORDLIST_FILENAME = "words.txt"
+    infile = open(WORDLIST_FILENAME)
+    line = infile.readline()
+    word_list=line.split()
+    return word_list
+    
+
+def choose_word():
+    """
+    word_list (list): list of words (strings)
+    ye function ek word randomly return karega
+    """
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+    return secret_word
+    
